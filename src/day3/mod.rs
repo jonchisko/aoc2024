7d6c75fdfa9e9b86c8cd579afdad1221ca912a87
DESKTOP-D6AFFRA\Jon <jonchisko@gmail.com>
2024-12-03 14:27:57 +0100

    Solve day 3 a case

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -1,6 +1,6 @@
 use crate::traits::Solution;
 
-struct Day3 {
+pub struct Day3 {
     command_data: String,
     pattern: String,
 }
@@ -9,7 +9,7 @@ impl Day3 {
     pub fn new() -> Day3 {
         Day3 {
             command_data: Day3::process_input(),
-            pattern: "mul(X, X)".to_string(),
+            pattern: "mul(X,X)".to_string(),
         }
     }
 
@@ -23,7 +23,9 @@ impl Day3 {
 }
 
 impl Solution for Day3 {
+    // "Non" regex solution
     fn solve1(&self) {
+        let mut numbers: Vec<u16> = vec![];
         let data_chars = self.command_data.chars().collect::<Vec<char>>();
         let pattern_chars = self.pattern.chars().collect::<Vec<char>>();
 
@@ -31,19 +33,52 @@ impl Solution for Day3 {
 
         while index < data_chars.len() {
             let mut pattern_index: usize = 0;
+            let mut candidate: Vec<String> = vec![];
 
             while pattern_index < pattern_chars.len() {
+                if pattern_chars[pattern_index] == 'X' {
+                    // try to read the 1-3 digit number
+                    let mut number: String = String::new();
+                    let mut digit_index: usize = 0;
+                    loop {
+                        if !data_chars[index + pattern_index + digit_index].is_numeric() {
+                            if number.len() > 0 {
+                                candidate.push(number);
+                            }
+                            break;
+                        }
+
+                        number.push(data_chars[index + pattern_index + digit_index]);
+                        digit_index += 1;
+                    }
+
+                    index += digit_index-1;
+                    pattern_index += 1;
+                }
+                
                 if data_chars[index + pattern_index] != pattern_chars[pattern_index] {
-                    index = index + pattern_index; // we might be checking one 'm' too many in this approach
+                    index = if data_chars[index + pattern_index] == 'm' {
+                        index + pattern_index
+                    } else {
+                        index + pattern_index + 1
+                    };
+
                     break;
                 }
-                // TODO: when special X character, read digits
-                // When pattern reading completed successfully -> store values to be multiplied in a vector?
+
                 pattern_index += 1;
             }
 
-            index += 1;
+            if pattern_index == pattern_chars.len() {
+                if candidate.len() == 2 && candidate[0].len() <= 3 && candidate[1].len() <= 3 {
+                    numbers.push(candidate[0].parse::<u16>().unwrap());
+                    numbers.push(candidate[1].parse::<u16>().unwrap());
+                }
+                index += pattern_index;
+            }
         }
+
+        println!("{:?}", numbers.iter().step_by(2).zip(numbers.iter().skip(1).step_by(2)).map(|pair| *pair.0 as u32 * *pair.1 as u32).sum::<u32>())
     }
 
     fn solve2(&self) {
